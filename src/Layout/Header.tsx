a374a87d834c12ee5ea119ebb76f5c7c67f1f9da
Suhail-007 <mohdsuhail51998@gmail.com>
2023-06-28 14:16:00 +0530

    added logo.svg and svg for manifest file

--- src/Layout/Header.tsx
+++ src/Layout/Header.tsx
@@ -1,13 +1,18 @@
 import styles from './header.module.css';
+import logo from '../../public/assets/logo.svg';
 
 export default function Header() {
-    <header className={styles.header}>
-        <div>
-            Icon
-        </div>
-        
-        <div>
-        sidebar
-        </div>
-    </header>
+    return (
+        <header className={styles.header}>
+            <div className={styles.header__icon}>
+                <svg>
+                    <use href='/assets/logo.svg#logo'></use>
+                </svg>
+            </div>
+
+            <div>
+                sidebar
+            </div>
+        </header>
+    )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Main from '../components/Main/main';
+import Header from '../Layout/Header';
 
 import styles from './page.module.css';
 
 export default function Home() {
     return (
-        <main className={styles.main}>
-            <Main />
-        </main>
+        <>
+            <Header />
+            <main className={styles.main}>
+                <Main />
+            </main>
+        </>
     )
 }
